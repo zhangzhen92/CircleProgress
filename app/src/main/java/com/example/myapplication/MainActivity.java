8b1afdb4bfbd35f4006f574040843e00b02f13e3
zz_dazzling@163.com <zz_dazzling@163.com>
2019-02-27 13:40:52 +0800

    demo

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.circlieprogress.CircleProgress;
+
+import java.lang.ref.WeakReference;
 
 public class MainActivity extends AppCompatActivity {
+    private MyHandler handler;
+    private int value = 1;
+    private CircleProgress circlieProgress;
+
+
+    private static class MyHandler extends Handler {
+        private WeakReference<MainActivity> mActivity;
+        public MyHandler(MainActivity activity){
+            mActivity = new WeakReference<MainActivity>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(mActivity != null && mActivity.get() != null){
+                mActivity.get().circlieProgress.setProgress(msg.what);
+                Message message = Message.obtain();
+                mActivity.get().value++;
+                if(mActivity.get().value > 100){
+                    return;
+                }
+                message.what = mActivity.get().value;
+                mActivity.get().handler.sendMessageDelayed(message,100);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler = new MyHandler(this);
+        circlieProgress = ((CircleProgress) findViewById(R.id.circle_progress));
+        Message message = Message.obtain();
+        message.what = value;
+        handler.sendMessage(message);
     }
 }
